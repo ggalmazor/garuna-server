6c86cb0dd727583c1a8da51cf0c94b35d5b02aa7
Guille <ggalmazor@gmail.com>
2018-01-29 22:16:23 +0100

    Add Koa router and demo

--- server.js
+++ server.js
@@ -1,18 +1,28 @@
-const Koa = require('koa');
-const app = new Koa();
-
 const {Client} = require('pg');
 const squel = require('squel').useFlavour('postgres');
 
+const Koa = require('koa');
+const app = new Koa();
+const Router = require('koa-router');
+const router = new Router();
 
-app.use(async ctx => {
-  console.log(ctx);
-  console.log("Serving request");
-  const res = await ctx.db.query(squel.select().from("consulta.demo").toString());
-  console.log("Fetch database rows");
-  ctx.body = res.rows;
-  console.log("Write response body");
+router.get('/hello/:name', ctx => ctx.body = `Hello ${ctx.params.name}`);
+
+router.post('/login', (ctx, next) => {
+
 });
+
+app.use(router.routes())
+    .use(router.allowedMethods());
+
+// use(async ctx => {
+//   console.log(ctx);
+//   console.log("Serving request");
+//   const res = await ctx.db.query(squel.select().from("consulta.demo").toString());
+//   console.log("Fetch database rows");
+//   ctx.body = res.rows;
+//   console.log("Write response body");
+// });
 
 const run = async () => {
   const db = new Client({
