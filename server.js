06c7d8ee0d6cb1360d29fd1623773c8f8f8aba0d
Guille <ggalmazor@gmail.com>
2018-01-29 21:57:18 +0100

    Add Sequel and PG, Demo PG

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const Koa = require('koa');
 const app = new Koa();
 
+const {Client} = require('pg');
+
+const client = new Client({
+  user: 'garuna',
+  host: 'pgsql02.dinaserver.com',
+  database: 'consulta',
+  password: 'F1s10G4run4',
+  port: 5432,
+});
+
+client.connect();
+
 app.use(async ctx => {
-  ctx.body = 'Hello World';
+  let res = await client.query("SELECT * FROM consulta.demo");
+  ctx.body = res.rows;
 });
 
 app.listen(13218);
